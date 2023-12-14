a233fbb0c800e848d1ea7bfa6112bccfa6681bf3
gopikannaps@gmail.com <gopikannaps@gmail.com>
2023-12-14 13:07:01 +0530

    all files

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('seo', function () {
+    return view('seo');
+});
+
+Route::get('components/about', function () {
+    return view('components/about');
+});
+
+
+// Route::view('./home','home');
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+// Route::get('./home',[Users::class,'Loadview']);
+
+// Route::get('home', [Users::class, 'index'])->name('user.index');
+
+// Route::get('home', [Users::class, 'home'])->name('user.contact');
